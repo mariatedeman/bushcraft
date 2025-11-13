cc5bc3509a20ed30e6bbd28162a198c369d0b6f5
mariatedeman <m.tedeman@hotmail.com>
2025-11-13 13:42:23 +0100

    New CSS library & components + media-queries, desktop header updated

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,10 @@
     <title>Document</title>
 
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/media-queries.css">
+    <link rel="stylesheet" href="styles/library-components.css">
+
+    <!-- FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Lexend+Exa:wght@100..900&display=swap" rel="stylesheet">
@@ -15,7 +19,16 @@
 
 <body>
 
+    <!-- DESKTOP -->
     <header>
+        <!-- <div class="icon-container-desktop">
+            <div class="icon-box"><a><img src="assets/hamburger-menu.svg"></a></div>
+            <div class="icon-box"><a><img src="assets/logotyp_header.svg"></a></div>
+            <div class="icon-box"><a><img src="assets/search.svg"></a></div>
+            <div class="icon-box"><a><img src="assets/smile.svg"></a></div>
+            <div class="icon-box"><a><img src="assets/shopping-cart.svg"></a></div>
+        </div> -->
+
         <div class="icon-container">
             <div class="icon-box"><a><img src="assets/hamburger-menu.svg"></a></div>
             <div class="icon-box"><a><img src="assets/search.svg"></a></div>
