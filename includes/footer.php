e1efb12fecd7f3f8e7134cf27ca40a4d7d7bf34c
OlofBjorn <olof.v.bjorn@gmail.com>
2025-11-24 20:52:24 +0100

    desktop footer wip, unfinished polish

--- includes/footer.php
+++ includes/footer.php
@@ -1,26 +1,54 @@
 <footer>
+<div id="footer-wrapper">   
+    <div id="footmedia1">
+        <div class="footer-more-info" id="footsupport">
+            <p class="body-bold">Support</p>
+            <img src="/assets/plus.svg" class="svgplus">
+            <!-- Class and P-tag below not displayed on mobile--->
+            <p class="footer-extra-info"> <br> Kontakt <br> Leverans <br> Retur <br> Garanti</p>
+        </div>
+        <div class="footer-more-info" id="footinfo">
+            <p class="body-bold">Information</p>
+            <img src="/assets/plus.svg" class="svgplus">
+            <!-- Class and P-tag below not displayed on mobile--->
+            <p class="footer-extra-info"> <br> Om oss <br> Karriär <br> Återförsäljare <br> Produktvård</p>
+        </div>
+        <div class="footer-more-info" id="footsek">
+            <p class="body-bold">Sekretess</p>
+            <img src="/assets/plus.svg" class="svgplus">
+            <!-- Class and P-tag below not displayed on mobile--->
+            <p class="footer-extra-info"> <br> Allmänna villkor <br> Policy</p> 
+        </div>
+    </div>
 
-    <div class="footer-more-info">
-        <p class="body-bold">Support</p>
-        <img src="/assets/plus.svg">
-    </div>
-    <div class="footer-more-info">
-        <p class="body-bold">Information</p>
-        <img src="/assets/plus.svg">
-    </div>
-    <div class="footer-more-info">
-        <p class="body-bold">Sekretess</p>
-        <img src="/assets/plus.svg">
+     <!-- Class and div below not displayed on mobile, image placeholder src--->
+    <div class="footer-logo">
+        <img src="/assets/logo-symbol-empty.svg">
     </div>
 
     <section class="footer-container">
         <div class="footer-socials">
             <p class="body-bold">Följ oss</p>
-            <span>
-                <img src="/assets/instagram.svg">
-                <img src="/assets/linkedin.svg">
-            </span>
+            <div>
+                <div class="footer-social-link" id="footer-socials-1">
+                    <img class="footer-social-img" src="/assets/instagram.svg">
+                    <!-- Class and P-tag below not displayed on mobile--->
+                    <p class="footer-extra-info">kinforma</p>
+                </div>
+                <div class="footer-social-link" id="footer-socials-2">
+                    <img class="footer-social-img" src="/assets/linkedin.svg">
+                    <!-- Class and P-tag below not displayed on mobile--->
+                    <p class="footer-extra-info">kinforma</p>
+                </div>
+            </div>
         </div>
+
+        <!-- Class and div below not displayed on mobile--->
+        <div class="footer-language">
+            <p id="footer-lang-setting">Sverige/SEK</p>
+            <img id="footer-lang-globe"src="/assets/globe.svg">
+        </div>
+
         <div class="footer-payment">
             <p class="body-bold">Betalningslösningar</p>
             <span>
@@ -30,6 +58,14 @@
             </span>
         </div>
     </section>
+</div>
+
+<div class="footer-copyright-container">
+    <div class="footer-copyright">
+        <img id="footer-copyright-icon"src="/assets/trademark.svg">
+        <p id="footer-copyright-text">KINFORMA</p>
+    </div>
+</div>
 
 </footer>
 
