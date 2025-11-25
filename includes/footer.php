37289e36323ad4bf4d40f441ee5b943091d24fd1
OlofBjorn <olof.v.bjorn@gmail.com>
2025-11-25 20:23:42 +0100

    Continued footer, potential polish needed

--- includes/footer.php
+++ includes/footer.php
@@ -1,44 +1,70 @@
 <footer>
-<div id="footer-wrapper">   
-    <div id="footmedia1">
-        <div class="footer-more-info" id="footsupport">
-            <p class="body-bold">Support</p>
-            <img src="/assets/plus.svg" class="svgplus">
-            <!-- Class and P-tag below not displayed on mobile--->
-            <p class="footer-extra-info"> <br> Kontakt <br> Leverans <br> Retur <br> Garanti</p>
-        </div>
-        <div class="footer-more-info" id="footinfo">
-            <p class="body-bold">Information</p>
-            <img src="/assets/plus.svg" class="svgplus">
-            <!-- Class and P-tag below not displayed on mobile--->
-            <p class="footer-extra-info"> <br> Om oss <br> Karriär <br> Återförsäljare <br> Produktvård</p>
-        </div>
-        <div class="footer-more-info" id="footsek">
-            <p class="body-bold">Sekretess</p>
-            <img src="/assets/plus.svg" class="svgplus">
-            <!-- Class and P-tag below not displayed on mobile--->
-            <p class="footer-extra-info"> <br> Allmänna villkor <br> Policy</p> 
+<div id="footer-wrapper">  
+    <div id=foot-media-flexwrap>     
+        <div id="footmedia1">
+            <!-- Class and div below not displayed on desktop -->
+            <div class="footer-more-info" id="footsupport-mobile">
+                <p class="body-bold">Support</p>
+                <img src="/assets/plus.svg" class="svgplus">
+            </div>
+            <!-- Class and div below not displayed on desktop -->
+            <div class="footer-more-info" id="footinfo-mobile">
+                <p class="body-bold">Information</p>
+                <img src="/assets/plus.svg" class="svgplus">
+            </div>
+            <div class="footer-more-info" id="footinfo-desktop">
+                <p class="body-bold">Information</p>
+                <img src="/assets/plus.svg" class="svgplus">
+                <!-- Class and P-tag below not displayed on mobile--->
+                <p class="footer-desktop-space"> <br></p>
+                <p class="footer-extra-info">Om oss <br></p>
+                <p class="footer-extra-info">Karriär <br></p>
+                <p class="footer-extra-info">Återförsäljare <br></p>
+                <p class="footer-extra-info">Produktvård</p>
+            </div>
+            <div class="footer-more-info" id="footsupport-desktop">
+                <p class="body-bold">Support</p>
+                <img src="/assets/plus.svg" class="svgplus">
+                <!-- Class and P-tag below not displayed on mobile--->
+                <p class="footer-desktop-space"> <br></p>
+                <p class="footer-extra-info">Kontakt <br></p>
+                <p class="footer-extra-info">Leverans <br></p>
+                <p class="footer-extra-info">Retur <br></p>
+                <p class="footer-extra-info">Garanti</p>
+            </div>
+            <div class="footer-more-info" id="footsek">
+                <p class="body-bold">Sekretess</p>
+                <img src="/assets/plus.svg" class="svgplus">
+                <!-- Class and P-tag below not displayed on mobile--->
+                <p class="footer-desktop-space"> <br></p> 
+                <p class="footer-extra-info">Allmänna villkor <br></p> 
+                <p class="footer-extra-info">Policy</p> 
+            </div>
         </div>
     </div>
 
      <!-- Class and div below not displayed on mobile, image placeholder src--->
-    <div class="footer-logo">
-        <img src="/assets/logo-symbol-empty.svg">
+    <div id="foot-logo-flexwrap">  
+        <div class="footer-logo">
+            <img src="/assets/logo-symbol-empty.svg">
+        </div>
     </div>
 
     <section class="footer-container">
-        <div class="footer-socials">
-            <p class="body-bold">Följ oss</p>
-            <div>
-                <div class="footer-social-link" id="footer-socials-1">
-                    <img class="footer-social-img" src="/assets/instagram.svg">
-                    <!-- Class and P-tag below not displayed on mobile--->
-                    <p class="footer-extra-info">kinforma</p>
-                </div>
-                <div class="footer-social-link" id="footer-socials-2">
-                    <img class="footer-social-img" src="/assets/linkedin.svg">
-                    <!-- Class and P-tag below not displayed on mobile--->
-                    <p class="footer-extra-info">kinforma</p>
+        <div id="foot-socials-flexwrap">
+            <div class="footer-socials">
+                <p class="body-bold">Följ oss</p>
+                <div>
+                    <div class="footer-social-link" id="footer-socials-1">
+                        <img class="footer-social-img" src="/assets/instagram.svg">
+                        <!-- Class and P-tag below not displayed on mobile--->
+                        <p class="footer-extra-soc">kinforma</p>
+                    </div>
+                    <div class="footer-social-link" id="footer-socials-2">
+                        <img class="footer-social-img" src="/assets/linkedin.svg">
+                        <!-- Class and P-tag below not displayed on mobile--->
+                        <p class="footer-extra-soc">kinforma</p>
+                    </div>
                 </div>
             </div>
         </div>
